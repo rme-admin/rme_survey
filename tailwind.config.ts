3cebdc445154da39e9c4d34500559b6ee34b658c
RME <rme.platform@gmail.com>
2026-03-27 18:01:36 +0000

    IN uin it is not updated everywhere correct it

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
         body: ['Poppins', 'sans-serif'],
         headline: ['Poppins', 'sans-serif'],
         code: ['monospace'],
